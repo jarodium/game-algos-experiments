19ac5497496cc9b6e5181315079be8b090422ab1
Pedro Heliodoro <jarodium@gmail.com>
2020-10-31 18:34:36 +0000

    dropped stargen conversion

--- universe/includes/stargen/stargen.php
+++
@@ -1,7 +0,0 @@
-<?php
-        include("_constants.php");
-        include("_structs.php");
-        include("_accrete.php");
-        /*include("_stargen.php");
-        include("_utils.php");*/
-?>
